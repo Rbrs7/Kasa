67d9de29d8d4b3d55d6595db4ab27678f82cdc54
Rbrs7 <raphael.bressi@wanadoo.fr>
2023-02-24 16:23:15 +0100

    collapse state ajouté + toutes les proptypes

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,14 +1,34 @@
 import arrow from "../assets/arrow.png";
 import "../styles/collapse.scss";
+import PropTypes from "prop-types";
+import React, { useState } from "react";
 
-export default function Collapse({ title, content }) {
+function Collapse({ title, content }) {
+  const [opened, setOpened] = useState(true);
   return (
     <div className="collapse">
-      <h2 className="collapse_title">
+      <h2
+        className="collapse_title"
+        onClick={() => {
+          setOpened(!opened);
+        }}
+      >
         {title}
         <img src={arrow} alt="display/hide content arrow" />
       </h2>
-      <div className="collapse_text">{content}</div>
+      <div
+        className="collapse_text"
+        style={opened ? { display: "block" } : { display: "none" }}
+      >
+        {content}
+      </div>
     </div>
   );
 }
+
+Collapse.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+};
+
+export default Collapse;
